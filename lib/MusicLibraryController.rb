9e51cab88a668de1ad4f89c8c906b5e411703aa2
Amit Singh <amit_nyc@live.com>
2018-07-26 18:21:09 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -82,6 +82,10 @@ def play_song
 puts "Which song number would you like to play?"
 input = gets.strip
 
+if song = Song.find_by_name(input)
+  list_songs
+end
+puts "Playing #{song.name} by #{song.artist.name}"
 end
 
 
